a8763dfd5c2cb281090710b5a7146a5143cb437f
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-02-21 08:54:44 +0000

    インデントサイズを他の実装と統一

--- astro/src/pages/api/fetchHTML.ts
+++ astro/src/pages/api/fetchHTML.ts
@@ -1,15 +1,16 @@
 import type { APIContext, APIRoute } from "astro";
 
 export const GET: APIRoute = async (req: APIContext) => {
-  const url = req.url.searchParams.get("url");
-  if (!url) {
-    return new Response("Missing URL parameter", { status: 400 });
-  }
-  try {
-    const html = await fetch(url).then((res) => res.text());
-    const res = new Response(html);
-    return res;
-  } catch (error) {
-    return new Response("Error fetching HTML", { status: 500 });
-  }
+    const url = req.url.searchParams.get("url");
+    if (!url) {
+        return new Response("Missing URL parameter", { status: 400 });
+    }
+    try {
+        const html = await fetch(url).then((res) => res.text());
+
+        const res = new Response(html);
+        return res;
+    } catch (error) {
+        return new Response("Error fetching HTML", { status: 500 });
+    }
 };
