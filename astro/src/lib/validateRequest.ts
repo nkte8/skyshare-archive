4b79fa8166a883f70844d7c2c393f30643c40c85
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-03-03 09:16:08 +0000

    🔒️ 不審なプロトコル、localhost、IPv4, 6アドレスと思わしき文字列を検出する機能を追加

--- astro/src/lib/validateRequest.ts
+++ astro/src/lib/validateRequest.ts
@@ -1,5 +1,15 @@
 import createErrResponse from "./createErrResponse";
 
+const protocol_validation: RegExp = /(dict|file|ftp|gopher|ldap|smtp|telnet|tftp):\/\//
+const loopback_validation: RegExp = /localhost/
+const ipv4_validation: RegExp = /(?:\d{0,3}\.){3}\d{0,3}/
+const ipv6_validation: RegExp = /\[[0-9a-fA-F:]+\]/
+
+/**
+ * デコード済みURLを返します。リクエストに問題がある場合はエラーレスポンスを返します。
+ * @param request リクエスト
+ * @returns デコード済みURL または エラーレスポンス
+ */
 const validateRequestReturnURL = ({
     request
 }: {
@@ -14,21 +24,30 @@ const validateRequestReturnURL = ({
     // GET以外はerror型で返却する
     if (request.method !== "GET") {
         return createErrResponse({
-            statusCode: 405
+            statusCode: 405,
         })
     }
+
     const url = new URL(request.url).searchParams.get("url");
     if (url === null) {
         return createErrResponse({
-            statusCode: 406
+            statusCode: 406,
         })
     }
+
+    const decodedUrl = decodeURIComponent(url)
     // SSRF対策
-    // if (url.toLowerCase().search(/localhost/) > 0) {
-    //     return createErrResponse({
-    //         statusCode: 502
-    //     })
-    // }
-    return url
+    if (
+        protocol_validation.test(decodedUrl) ||
+        loopback_validation.test(decodedUrl) ||
+        ipv4_validation.test(decodedUrl) ||
+        ipv6_validation.test(decodedUrl)
+    ) {
+        return createErrResponse({
+            statusCode: 502,
+        })
+    }
+
+    return decodedUrl
 }
 export default validateRequestReturnURL
